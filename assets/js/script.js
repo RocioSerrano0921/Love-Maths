b5d9a460facdfac92b6e7bdc2fdcd7415c9f2334
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-08-11 20:15:49 +0200

    Set up structure to game functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame() {}
+
+function checkAnswers() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
