82543ccb4bfca982cc4baba4ccc6963462c88103
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-08-12 17:08:56 +0200

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,13 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
+
+    document.getElementById('answer-box').addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            checkAnswers();
+        }
+    });
+
     runGame('addition'); // Start the game with addition
 });
 
@@ -26,6 +33,9 @@ document.addEventListener('DOMContentLoaded', function () {
  * and after the user answer has been processed
  */
 function runGame(gameType) {
+    document.getElementById('answer-box').value = ''; // Clear the answer box
+    document.getElementById('answer-box').focus(); // Focus on the answer box, with the cursor placed inside
+
     // Generates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
